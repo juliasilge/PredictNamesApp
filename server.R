0c96b1d8b48b692d10de6c2ebc7e168ab4277e7b
juliasilge <julia.silge@gmail.com>
2016-02-26 15:59:28 -0700

    Maybe commenting code is a good idea

--- server.R
+++ server.R
@@ -10,22 +10,34 @@ shinyServer(function(input, output) {
         output$myPlot <- renderPlot({
                 if (input$sex == "female") babysex <- "F"
                 else if (input$sex == "male") babysex <- "M"
+
+                # fix capitalization of name
                 ourname <- paste0(toupper(substring(tolower(input$chosenname), 1, 1)), 
                                   substring(tolower(input$chosenname), 2))
                 pickaname <- babynames %>% filter(sex == babysex, name == ourname)
+
+                # what is the proportion of the original name that we want to match?                
                 goalprop <- as.numeric(pickaname[pickaname$year == input$firstyear, 'prop'])
                 subsetfitname <- pickaname %>% 
                         filter(year %in% seq(input$firstyear-5,input$firstyear+5))
+
+                # fit to just 10 years around first year to find slope of original name
                 myfit <- lm(prop ~ year, subsetfitname)
                 goalslope <- myfit$coefficients[2]
+                
+                # what names match the original name in proportion at the goal year?
                 findmatches <- babynames %>% filter(sex == babysex, 
                                                     year == input$goalyear, 
                                                     prop < goalprop*1.1 & prop > goalprop*0.9) %>%
                         mutate(slope = 0.00)
+                
+                # if there are a lot matches, sort the matches and only keep the first 60
                 if (dim(findmatches)[1] > 60) {
                         findmatches <- findmatches[order(abs(goalprop - findmatches$prop)),]
                         findmatches <- findmatches[1:60,]
                 }
+                
+                # for each matching name, calculate the slope at the goal year
                 for (i in seq_along(findmatches$name)) {
                         matchfitname <- babynames %>% filter(sex == babysex, 
                                                              name == as.character(findmatches[i,'name']))
@@ -34,6 +46,8 @@ shinyServer(function(input, output) {
                         matchfit <- lm(prop ~ year, matchfitname)
                         findmatches[i,'slope'] <- matchfit$coefficients[2]
                 }
+                
+                # which of the matching names match the original name in slope?
                 if (goalslope >= 0.00005) {
                         matchnames <- findmatches %>% filter(slope >= 0.00005) %>% select(name)
                 } else if (goalslope <= -0.00005) {
@@ -43,6 +57,7 @@ shinyServer(function(input, output) {
                                 filter(slope > -0.00005 & slope < 0.00005) %>% select(name) 
                 }
                 
+                # THE MATCHES!
                 matchnames <- babynames %>% filter(sex == babysex, name %in% matchnames$name)
                 plotname <- rbind(pickaname, matchnames)
                 ggplot(plotname, aes(x = year, y = prop, color = name)) + 
